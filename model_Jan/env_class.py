5731711b66b2d86208fee2665024f909ee691f9c
Jan <jan.leisbrock@web.de>
2024-04-12 15:25:01 +0200

    sexy andrew

--- model_Jan/env_class.py
+++ model_Jan/env_class.py
@@ -5,15 +5,17 @@ from gym.utils import seeding
 class BatteryManagementEnv(Env):
 
     def __init__(self):
-        self.charge_level = 50  # Initial battery charge level
-        self.action_space = spaces.MultiDiscrete([3] * 24)  # 24-hour action vector with actions -1, 0, or 1
-        self.observation_space = spaces.Box(low=np.array([0]), high=np.array([100]), dtype=np.float32)
+        self.charge_level = 0  # Initial battery charge level
+        self.action_space = 1  # 24-hour action vector with actions -1, 0, or 1
+        self.observation_space = 1  # Battery charge level as a float
 
         # Set other needed variables like the charge rate, discharge rate, etc.
         self.charge_rate = 10  # Amount of charge to increase per step when charging
         self.discharge_rate = 10  # Amount of charge to decrease per step when discharging
         self.max_charge = 100
         self.min_charge = 0
+
+        self.nA = 2
 
         self.seed()
         self.reset()
@@ -27,12 +29,22 @@ class BatteryManagementEnv(Env):
         self.charge_level = 50  # Or some other logic to determine initial charge level
         return np.array([self.charge_level]).astype(np.float32)
 
-    def step(self, action_vector):
+    def step(self, action):
+
+        action_1 = [1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0]
+        action_2 = [0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1]
+
+        if action == 0:
+            selected_action = action_1
+        elif action == 1:
+            selected_action = action_2
+        
+
         total_reward = 0
 
-        for hour_action in action_vector:
+        for hour_action in selected_action:
             reward = 0
-            if hour_action == 2:  # charge
+            if hour_action == 1:  # charge
                 new_charge_level = self.charge_level + self.charge_rate
                 if new_charge_level < self.max_charge:
                     self.charge_level = new_charge_level
@@ -46,19 +58,8 @@ class BatteryManagementEnv(Env):
                     self.charge_level = new_charge_level
                     reward += 10
                 else:
-                    reward += -100
-
-            elif hour_action == 1:  # wait
-                reward += 0
+                    reward += -100  # Penalty for trying to overdischarge
 
             total_reward += reward
 
-        info = {}
-
-        return np.array([self.charge_level]).astype(np.float32), action_vector, total_reward, info
-    
-from gym.envs.registration import register
-
-register(
-     id='BatteryManagementEnv',
-     entry_point='C:/Users/janle/Desktop/Master_local/2/AdvancedBA/model_Jan/env_class:BatteryManagementEnv',)
+        return np.array([self.charge_level]).astype(np.float32), total_reward
